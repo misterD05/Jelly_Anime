4004c7094a05fcd5d5bf1d410e5e45931e98852d
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-15 22:51:54 +0200

    animation adjust

--- JS/animations.js
+++ JS/animations.js
@@ -1,9 +1,6 @@
 var container = document.getElementById('menuBar');
 var drone = document.getElementById('drone');
 
-drone.style.position = "absolute";
-drone.style.top= "50%";
-drone.style.left= "50%";
 
 container.addEventListener("mousemove", function(event) {
   drone.style.transform = `translate3d(${(event.offsetX)}px, ${(event.offsetY )}px, 0)`;
