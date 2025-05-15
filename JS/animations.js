9d0b24b8a7854c81447f83ab29f5184337a16bcd
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-15 17:37:00 +0200

    try animations

---
+++ JS/animations.js
@@ -0,0 +1,10 @@
+var container = document.getElementById('menuBar');
+var drone = document.getElementById('drone');
+var droneCenter = {
+  x: drone.width() / 2,
+  y: drone.height() / 2
+};
+
+container.on('mousemove', function(event) {
+  drone.css('transform', `translate3d(${event.offsetX - droneCenter.x}px, ${event.offsetY - droneCenter.y}px, 0)`);
+});
