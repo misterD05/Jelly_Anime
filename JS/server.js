a82a6c39dba8d3c41a12ad9228a2c4889b9f78ce
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-16 23:16:30 +0200

    try console log info

--- JS/server.js
+++ JS/server.js
@@ -3,6 +3,10 @@ let url = "https://api.jikan.moe/v4/characters";
 
 let response = fetch(url).then(function (response){return response.json()});
 
-console.log(response);
+
+for(let c of response.data){
+    console.log(c.name);
+    
+}
 
 let strut = "<div class='character'><img class='profile' src='' alt=''><div class='nameC'>Name</div></div>"
