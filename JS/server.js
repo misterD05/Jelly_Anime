018a01a9a338aab132a9a0900b8eed1ea7de9a54
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-16 23:07:30 +0200

    try catch infos

---
+++ JS/server.js
@@ -0,0 +1,8 @@
+let charactersContainer = document.getElementById('characters');
+let url = "https://api.jikan.moe/v4/characters";
+
+let response = await fetch(url).then(function (response){return response.json()});
+
+console.log(response);
+
+let strut = "<div class='character'><img class='profile' src='' alt=''><div class='nameC'>Name</div></div>"
