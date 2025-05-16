562035d7f8dbc1086c038707cb718a26cefbf890
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-16 23:39:16 +0200

    another try

--- JS/server.js
+++ JS/server.js
@@ -3,8 +3,16 @@ let url = "https://api.jikan.moe/v4/characters";
 
 let response = fetch(url).then(function (response){return response.json()});
 
-console.log(response.data.json());
-let data = JSON.parse(response.data);
+
+
+let obj = eval('(' + response + ')');
+
+let data = [];
+
+for (let i in obj)
+    data.push(obj[i].json());
+
+console.log(res);
 
 for(let i = 0; i < data.length; i++){
     console.log(response.data[i].name);
