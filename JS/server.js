3e5ecbaa65eb2eff6b9f666f417d4329ce366ea1
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-17 19:51:59 +0200

    try to put infos

--- JS/server.js
+++ JS/server.js
@@ -5,7 +5,11 @@ async function getCharacters() {
     let response = await fetch(url).then(response => response.json()).then(data => {
     const characters = data.data;
     characters.forEach(character => {
-      console.log(character.name);
+        let imageUrl = character.images;
+        let struct = document.createElement("div");
+        struct.className = 'character';
+        struct.innerHTML = "<img class='profile' src=" + imageUrl.jpg.image_url + " alt=''><div class='nameC'>" + character.name + "</div>"
+        charactersContainer.appendChild(struct)  
     });
   })
 
