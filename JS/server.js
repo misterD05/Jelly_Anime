ae1aa706db176fcfbf7e151d3a8bee0a9b9bbc83
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-17 18:23:11 +0200

    try async function

--- JS/server.js
+++ JS/server.js
@@ -1,12 +1,15 @@
 let charactersContainer = document.getElementById('characters');
 let url = "https://api.jikan.moe/v4/characters";
 
-let response = fetch(url).then(response => response.json()).then(data => console.log(data));
+async function getCharacters() {
+    let response = await fetch(url).then(response => response.json()).then(data => console.log(data));
 
+    console.log(response.data)
 
-for(let i = 0; i < response.data.length; i++){
-    console.log("data " + i + " " + response.data[i].name);
-    
+    for(let i = 0; i < response.data.length; i++){
+        console.log("data " + i + " " + response.data[i].name);
+
+    }
 }
 
 let strut = "<div class='character'><img class='profile' src='' alt=''><div class='nameC'>Name</div></div>"
