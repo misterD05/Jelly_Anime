ddf07ca6ec57ffb81406f5243e4e8424cb032f33
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-17 19:49:40 +0200

    try to read

--- JS/server.js
+++ JS/server.js
@@ -2,18 +2,13 @@ let charactersContainer = document.getElementById('characters');
 let url = "https://api.jikan.moe/v4/characters";
 
 async function getCharacters() {
-    let response = await fetch(url).then(response => response.json()).then(function (data){return data.json});
+    let response = await fetch(url).then(response => response.json()).then(data => {
+    const characters = data.data;
+    characters.forEach(character => {
+      console.log(character.name);
+    });
+  })
 
-    console.log(response)
-
-    for(let item  of response){
-      let character = item;
-      let imageUrl = item.images;
-      let struct = document.createElement("div");
-      struct.className = 'character';
-      struct.innerHTML = "<img class='profile' src=" + imageUrl.jpg.image_url + " alt=''><div class='nameC'>" + character.name + "</div>"
-      charactersContainer.appendChild(struct)  
-    }
 
 }
 
