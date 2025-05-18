b403f7cf38ff1a261dc5a8f391273c290841c804
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 10:59:10 +0200

    try adjust script

--- JS/game.js
+++ JS/game.js
@@ -1,31 +1,9 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 let guess;
 let url = "https://api.jikan.moe/v4/characters";
-
 let numCharacters;
 
-async function forNumC(){
-    let response = await fetch(url).then(response => response.json()).then(data => {console.log(data.pagination.items.total); numCharacters = data.pagination.items.total;}).catch(error => console.error('Error:', error));
-}
 
-async function getCharacter(urlM) {
-    let response = await fetch(urlM).then(response => response.json()).then(data => {
-    
-    const character = data.data;
-    
-        guess = new Character(character.name, character.name_kanji, character.about, character.images)
-        imgGuess.src = character.images.jpg.image_url;
-    }).catch(error => console.error('Error:', error));
-
-
-}
-
-
-
-async function getRandomCharacter() {
+function getRandomCharacter() {
     description.style.visibility = "hidden";
     newGame.style.visibility = "hidden";
     forNumC();
@@ -43,7 +21,7 @@ let description = document.getElementById("description");
 let answer = document.getElementById("nameTry");
 
 document.addEventListener("keydown", function(event){
-    if(event.keycode == 13){
+    if(event.key === 'Enter'){
         if(nameTry.value == guess.name){
             description.innerHTML += guess.nam + "\n" +guess.name_kanji + "\n" + guess.nicknames + "\n" + guess.about;
             description.style.visibility = "visible";
@@ -57,6 +35,30 @@ let newGame = document.getElementById("newGame");
 newGame.addEventListener("click", function(){
     getRandomCharacter();
 })
+
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function forNumC(){
+    let response = await fetch(url).then(response => response.json()).then(data => { numCharacters = data.pagination.items.total;}).catch(error => console.error('Error:', error));
+    console.log(numCharacters);
+}
+
+async function getCharacter(urlM) {
+    let response = await fetch(urlM).then(response => response.json()).then(data => {
+    
+    const character = data.data;
+    
+        guess = new Character(character.name, character.name_kanji, character.about, character.images)
+        imgGuess.src = character.images.jpg.image_url;
+    }).catch(error => console.error('Error:', error));
+
+}
+
+
 
 class Character{
     constructor(name, name_kanji, nicknames, about, images){
