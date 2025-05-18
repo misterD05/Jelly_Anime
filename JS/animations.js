939ab93f9ec13f3925e8b8bd6436c2a5a0b1a18b
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 18:50:04 +0200

    check

--- JS/animations.js
+++ JS/animations.js
@@ -27,6 +27,7 @@ function transformElement(x, y,i) {
 mouseOverContainer.addEventListener("mousemove", (e) => {
     window.requestAnimationFrame(function () {
         for(let i = 0; i < element.length; i++){
+          console.log(e.clientX >= element[i].left && e.clientX <= element[i].right && e.clientY >= element[i].top && e.clientY <= element[i].bottom);
           if (e.clientX >= element[i].left && e.clientX <= element[i].right && e.clientY >= element[i].top && e.clientY <= element[i].bottom){
             transformElement(e.clientX, e.clientY, i);
           }
