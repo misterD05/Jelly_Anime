8a69d4fa562e4483cb7a62d2f675453deb13cfc5
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 12:08:49 +0200

    correction

--- JS/game.js
+++ JS/game.js
@@ -19,7 +19,7 @@ let answer = document.getElementById("nameTry");
 document.addEventListener("keydown", function(event){
     if(event.key === 'Enter'){
         if(nameTry.value == guess.name){
-            description.innerHTML = "<summary id="butSum">Description</summary>" +  guess.name + "\n" +guess.name_kanji + "\n" + guess.nicknames + "\n" + guess.about;
+            description.innerHTML = "<summary id='butSum'>Description</summary>" +  guess.name + "<br>" +guess.name_kanji + "<br>" + guess.nicknames + "<br>" + guess.about;
             description.style.visibility = "visible";
             newGame.style.visibility = "visible";
         }
