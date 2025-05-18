bd6df1ae074c1743a000f3a5ded192ad55b6ff05
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 19:30:15 +0200

    try to solve the bug

--- JS/animations.js
+++ JS/animations.js
@@ -92,9 +92,9 @@ if(mouseOverContainer2 != null){
 function getCoords(elem) {
     let box = elem.getBoundingClientRect();
     return {
-        top: box.top + window.pageYOffset,
-        right: box.right + window.pageXOffset,
-        bottom: box.bottom + window.pageYOffset,
-        left: box.left + window.pageXOffset
+        top: box.top,
+        right: box.right
+        bottom: box.bottom,
+        left: box.left
     };
 }
