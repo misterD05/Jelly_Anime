c43900fc33e201099963d7e9e5f2eb898b86ed56
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 12:43:23 +0200

    correction animation

--- JS/animations.js
+++ JS/animations.js
@@ -16,22 +16,27 @@ const multiple = 10;
 const mouseOverContainer = document.getElementById("characters");
 const element = document.getElementsByClassName("character");
 
-function transformElement(x, y) {
-    let box = element.getBoundingClientRect();
+function transformElement(x, y,i) {
+    let box = element[i].getBoundingClientRect();
     const calcX = -(y - box.y - box.height / 2) / multiple;
     const calcY = (x - box.x - box.width / 2) / multiple;
     const percentage = parseInt((x - box.x) / box.width * 1000) / 10;
-    element.style.transform = "rotateX(" + calcX + "deg) rotateY(" + calcY + "deg)";
+    element[i].style.transform = "rotateX(" + calcX + "deg) rotateY(" + calcY + "deg)";
 }
 
 mouseOverContainer.addEventListener("mousemove", (e) => {
     window.requestAnimationFrame(function () {
-        transformElement(e.clientX, e.clientY);
+        for(let i = 0; i < element.length; i++){
+          transformElement(e.clientX, e.clientY, i);
+        }
     });
 });
 
 mouseOverContainer.addEventListener("mouseleave", (e) => {
     window.requestAnimationFrame(function () {
-        element.style.transform = "rotateX(0) rotateY(0)";
+      for(let i = 0; i < element.length; i++){
+          element[i].style.transform = "rotateX(0) rotateY(0)";
+          
+        }
     });
 });
