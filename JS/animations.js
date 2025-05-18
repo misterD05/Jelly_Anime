8094ef80f93975e3fc5d0dfdceba3ffd44f86c32
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-18 12:40:09 +0200

    try animation card

--- JS/animations.js
+++ JS/animations.js
@@ -11,3 +11,27 @@ window.addEventListener('resize', function () {
     window.location.reload();
 });
 
+
+const multiple = 10;
+const mouseOverContainer = document.getElementById("characters");
+const element = document.getElementsByClassName("character");
+
+function transformElement(x, y) {
+    let box = element.getBoundingClientRect();
+    const calcX = -(y - box.y - box.height / 2) / multiple;
+    const calcY = (x - box.x - box.width / 2) / multiple;
+    const percentage = parseInt((x - box.x) / box.width * 1000) / 10;
+    element.style.transform = "rotateX(" + calcX + "deg) rotateY(" + calcY + "deg)";
+}
+
+mouseOverContainer.addEventListener("mousemove", (e) => {
+    window.requestAnimationFrame(function () {
+        transformElement(e.clientX, e.clientY);
+    });
+});
+
+mouseOverContainer.addEventListener("mouseleave", (e) => {
+    window.requestAnimationFrame(function () {
+        element.style.transform = "rotateX(0) rotateY(0)";
+    });
+});
